5654acd6e648888072071961375f2a5fcc980f3c
Yagnik Sangani <yagniksangani95@gmail.com>
2022-05-03 20:53:43 +0530

    Fixed the code and problems with render_callback() function

--- php/Block.php
+++ php/Block.php
@@ -63,66 +63,85 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
+		$html       = '';
+		$post_types = get_post_types( array( 'public' => true ) );
 		$class_name = $attributes['className'];
 		ob_start();
 
 		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+		<div class="<?php echo esc_attr( $class_name ); ?>">
+			<h2><?php echo esc_html( __( 'Post Counts', 'site-counts' ) ); ?></h2>
 			<ul>
-			<?php
+		<?php
+		if ( ! empty( $post_types ) ) :
 			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
+				$post_type_object = get_post_type_object( $post_type_slug );
+				$post_count       = count(
+					get_posts(
+						array(
+							'post_type'      => $post_type_slug,
 							'posts_per_page' => -1,
-						]
+						)
 					)
-                );
+				);
 
+				/* translators: %1$d post count, %2$s label name */
+				$count_text = sprintf( __( 'There are %1$d %2$s.', 'site-counts' ), $post_count, $post_type_object->labels->name );
 				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+					<li><?php echo esc_html( $count_text ); ?></li>
+				<?php
+				endforeach;
+				endif;
+		?>
+			</ul>
+			<p>
+			<?php
+			/* translators: %d post id */
+			$count_text = sprintf( __( 'The current post ID is %d.', 'site-counts' ), $_GET['post_id'] );
+			echo esc_html( $count_text );
+			?>
+			</p>
 
 			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
+			$query = new WP_Query(
+				array(
+					'post_type'     => array( 'post', 'page' ),
+					'post_status'   => 'any',
+					'date_query'    => array(
+						array(
+							'hour'    => 9,
+							'compare' => '>=',
+						),
+						array(
+							'hour'    => 17,
+							'compare' => '<=',
+						),
 					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-			));
+					'tag'           => 'foo',
+					'category_name' => 'baz',
+					'post__not_in'  => array( get_the_ID() ),
+				)
+			);
 
 			if ( $query->have_posts() ) :
 				?>
-				 <h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
+				<h2><?php echo esc_html( __( '5 posts with the tag of foo and the category of baz', 'site-counts' ) ); ?></h2>
+				<ul>
+				<?php
 
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
+				foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
+					?>
+					<li><?php echo esc_html( $post->post_title ); ?></li>
+					<?php
 				endforeach;
 			endif;
-		 	?>
+			?>
 			</ul>
 		</div>
 		<?php
 
-		return ob_get_clean();
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
 	}
 }
